a1926976c7895b022cabf5d6b948fe9d06b1f42c
purushottam <purushottam3366@gmail.com>
2024-02-18 02:32:29 +0530

    changes swagger

--- StudentAPI/Program.cs
+++ StudentAPI/Program.cs
@@ -18,11 +18,11 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"))
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseAuthorization();
 
